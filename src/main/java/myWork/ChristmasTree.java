75c1a0c9220ab6052ea68dbefa15254ce0c31962
Partizanin <topiciv1@yandex.ua>
2014-10-28 13:51:47 +0200

    new class

---
+++ src/main/java/myWork/ChristmasTree.java
@@ -0,0 +1,28 @@
+package myWork;
+
+/**
+ * Created with Intellij IDEA.
+ * Project name: Partizanin
+ * User: Partizanin
+ * Date: 28.10.2014
+ * Time:  13:49
+ * To change this template use File|Setting|File Templates.
+ */
+public class ChristmasTree {
+
+    public static void main(String[] args) {
+        triangle(12, "*");
+    }
+
+    private static void triangle(int n, String value) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                System.out.print(value);
+            }
+            System.out.println("");
+        }
+    }
+}
