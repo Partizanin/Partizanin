c3d763b66b46cb7023bdd7e629c154ab87672e8e
Partizanin <topiciv1@yandex.ua>
2014-11-10 21:47:38 +0200

    test git integration

---
+++ src/main/java/myWork/other/CharTest.java
@@ -0,0 +1,17 @@
+package myWork.other;
+
+/**
+ * Created with Intellij IDEA.
+ * Project name: ${PROJECT_NAME}.
+ * User: Partizanin.
+ * Date: 10.11.2014.
+ * Time:  21:07.
+ * To change this template use File|Setting|File Templates.
+ */
+public class CharTest {
+
+    public static void main(String[] args) {
+        char s = '\u00bb';
+        System.out.println(s);
+    }
+}
